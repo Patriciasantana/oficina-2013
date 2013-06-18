b7ad28405f8f255babf1b6014101d0daa5ea5431
patricia-cardoso <patricia-cardoso@patriciacardoso-HP-G42-Notebook-PC.(none)>
2013-06-18 12:01:46 -0300

    inicializando projeto

---
+++ oficina/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oficina::Application.config.secret_token = '33626c8770e1cd8d2912e170b2ed71b945aa3b45d28828bbec6616efec206870b52d050df8120a8c5df274123dfc60de4a27d02d22bc9d018781f86c8ce323f4'
